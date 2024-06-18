837f2a41264ccd750fed6e7e56e69723c15074a4
Lucie Yang <luya.495@gmail.com>
2024-06-18 00:46:17 -0400

    added swiping functionality

--- HokkienTranslation/screens/FlashcardScreen.js
+++ HokkienTranslation/screens/FlashcardScreen.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Box, Text, Button, Center, VStack } from "native-base";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, Animated, PanResponder } from "react-native";
 
 const FlashcardScreen = () => {
   const [showTranslation, setShowTranslation] = useState(false);
@@ -13,6 +13,35 @@ const FlashcardScreen = () => {
   ];
 
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const position = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gestureState) => {
+        position.setValue({ x: gestureState.dx, y: gestureState.dy });
+      },
+      onPanResponderRelease: (event, gestureState) => {
+        if (gestureState.dx > 120 || gestureState.dx < -120 || gestureState.dy > 120 || gestureState.dy < -120) {
+          Animated.timing(position, {
+            toValue: { x: gestureState.dx > 0 ? 500 : -500, y: gestureState.dy > 0 ? 500 : -500 },
+            duration: 500,
+            useNativeDriver: true,
+          }).start(() => {
+            setShowTranslation(false);
+            setCurrentCardIndex((prevIndex) => (prevIndex + 1) % flashcards.length);
+            position.setValue({ x: 0, y: 0 });
+          });
+        } else {
+          Animated.spring(position, {
+            toValue: { x: 0, y: 0 },
+            useNativeDriver: true,
+          }).start();
+        }
+      },
+    })
+  ).current;
+  
 
   const handleFlip = () => {
     setShowTranslation(!showTranslation);
@@ -27,21 +56,30 @@ const FlashcardScreen = () => {
     <Center flex={1} px="3">
       <VStack space={4} alignItems="center">
         <TouchableOpacity onPress={handleFlip}>
-          <Box
-            width="300px"
-            height="200px"
-            bg="primary.500"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="10px"
-            shadow={2}
+          <Animated.View {...panResponder.panHandlers} 
+            style={[position.getLayout(), 
+            { transform: [{ rotate: position.x.interpolate({
+              inputRange: [-500, 0, 500],
+              outputRange: ['-10deg', '0deg', '10deg']
+            }) }] }
+          ]}
           >
-            <Text fontSize="2xl" color="white">
-              {showTranslation
-                ? flashcards[currentCardIndex].translation
-                : flashcards[currentCardIndex].word}
-            </Text>
-          </Box>
+            <Box
+              width="300px"
+              height="200px"
+              bg="primary.500"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="10px"
+              shadow={2}
+            >
+              <Text fontSize="2xl" color="white">
+                {showTranslation
+                  ? flashcards[currentCardIndex].translation
+                  : flashcards[currentCardIndex].word}
+              </Text>
+            </Box>
+          </Animated.View>
         </TouchableOpacity>
         <Button onPress={handleNext}>Next</Button>
       </VStack>
