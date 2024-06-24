55383a3bf1e8ba02ef9b1c8827426299fd3064f7
tasmimabrar <tasmimabrar@gmail.com>
2024-06-24 13:17:57 -0700

    feat: added 'back to categories' button, and realigned the buttons to be more fitting

--- HokkienTranslation/screens/FlashcardScreen.js
+++ HokkienTranslation/screens/FlashcardScreen.js
@@ -25,43 +25,54 @@ const FlashcardScreen = () => {
   };
 
   return (
-    <Center flex={1} px="3" background={colors.surface}>
-      <VStack space={4} alignItems="center">
-        <HStack width="100%" justifyContent="space-between" alignItems="center">
-          <Box />
-          <Button
+    <Box flex={1} background={colors.surface}>
+      <HStack alignItems="center" justifyContent="flex-start" p={4}>
+        <Button
+          marginRight="auto"
+          onPress={() => navigation.goBack()}
+          background={colors.primaryContainer}
+          _text={{ color: colors.onSurface }}
+        >
+          Back to Categories
+        </Button>
+        <Button 
+            marginLeft="auto"
             onPress={() => navigation.navigate('Quiz')}
             background={colors.primaryContainer}
             _text={{ color: colors.onSurface }}
+            
           >
             Test your abilities
           </Button>
-        </HStack>
-        <TouchableOpacity onPress={handleFlip}>
-          <Box
-            width="300px"
-            height="200px"
-            bg={colors.primaryContainer}
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="10px"
-            shadow={2}
-          >
-            <Text fontSize="2xl" color={colors.onSurface}>
-              {showTranslation
-                ? flashcards[currentCardIndex].translation
-                : flashcards[currentCardIndex].word}
-            </Text>
-          </Box>
-        </TouchableOpacity>
+      </HStack>
+      <Center flex={1} px="3">
+        <VStack space={4} alignItems="center">
+          <TouchableOpacity onPress={handleFlip}>
+            <Box
+              width="300px"
+              height="200px"
+              bg={colors.primaryContainer}
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="10px"
+              shadow={2}
+            >
+              <Text fontSize="2xl" color={colors.onSurface}>
+                {showTranslation
+                  ? flashcards[currentCardIndex].translation
+                  : flashcards[currentCardIndex].word}
+              </Text>
+            </Box>
+          </TouchableOpacity>
           <FlashcardNavigator
             currentCardIndex={currentCardIndex}
             flashcardsLength={flashcards.length}
             setCurrentCardIndex={setCurrentCardIndex}
             setShowTranslation={setShowTranslation}
           />
-      </VStack>
-    </Center>
+        </VStack>
+      </Center>
+    </Box>
   );
 };
 
