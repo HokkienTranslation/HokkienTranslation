ea6b4147c188afabf65dae1f7a86fafc1dc17493
tasmimabrar <tasmimabrar@gmail.com>
2024-06-25 06:32:52 -0700

    fix: minor modifications

--- HokkienTranslation/screens/QuizScreen.js
+++ HokkienTranslation/screens/QuizScreen.js
@@ -6,12 +6,13 @@ const QuizScreen = () => {
   const { theme, themes } = useTheme();
   const colors = themes[theme];
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
-
+  
+  //TODO: Change the positions of answers to be random
   const flashcards = [
-    { word: "Apple", choices: ["Banana", "Orange", "Apple", "Grape"] },
-    { word: "Banana", choices: ["Peach", "Banana", "Berry", "Melon"] },
-    { word: "Cat", choices: ["Dog", "Bird", "Fish", "Cat"] },
-    { word: "Dog", choices: ["Cat", "Dog", "Rabbit", "Horse"] },
+    { word: "Apple", choices: ["Banana", "Orange", "Apple", "Grape"], answer: 2 },
+    { word: "Banana", choices: ["Peach", "Banana", "Berry", "Melon"], answer: 1 },
+    { word: "Cat", choices: ["Dog", "Bird", "Fish", "Cat"], answer: 3 },
+    { word: "Dog", choices: ["Cat", "Dog", "Rabbit", "Horse"], answer: 1 },
   ];
 
   const handleChoice = () => {
@@ -22,13 +23,13 @@ const QuizScreen = () => {
     <Center flex={1} px="3" background={colors.surface}>
       <VStack space={4} alignItems="center">
         <Box
-          width="350px" // Adjust width as needed
-          height="250px" // Adjust height as needed
+          width="350px"
+          height="250px"
           bg={colors.primaryContainer}
           borderRadius="10px"
           shadow={2}
           p={4}
-          justifyContent="center" // Center content vertically
+          justifyContent="center"
         >
           <VStack space={4} alignItems="center" flex={1} justifyContent="center">
             <Text fontSize="2xl" color={colors.onSurface} mb={4}>
