6b70a2bb60b9dc6eee56437eb72e44919d37d5f8
Vincent Zhu <vincenthero16@gmail.com>
2024-06-24 20:27:00 -0400

    feat: Randomized words from db display

--- HokkienTranslation/screens/HomeScreen.js
+++ HokkienTranslation/screens/HomeScreen.js
@@ -1,17 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
-import { Box, Input, IconButton, ScrollView, VStack } from "native-base";
+import {
+  Box,
+  Input,
+  IconButton,
+  ScrollView,
+  VStack,
+  Button,
+  Wrap,
+  Flex,
+  Text,
+} from "native-base";
 import { useTheme } from "./context/ThemeProvider";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../backend/database/Firebase";
 
 export default function HomeScreen({ navigation }) {
-  const [query, setQuery] = useState("");
+  const [queryText, setQueryText] = useState("");
+  const [randomInputs, setRandomInputs] = useState([]);
   const { theme, themes } = useTheme();
   const colors = themes[theme];
 
   const handleTextChange = (text) => {
     const cleanedText = text.replace(/\n/, "");
-    setQuery(cleanedText);
+    setQueryText(cleanedText);
   };
+
+  const fetchRandomInputs = async () => {
+    try {
+      const translationCollection = collection(db, "translation");
+      const querySnapshot = await getDocs(translationCollection);
+      const inputs = querySnapshot.docs.map((doc) => doc.data().englishInput);
+      setRandomInputs(inputs.sort(() => 0.5 - Math.random()).slice(0, 5));
+    } catch (error) {
+      console.error("Error fetching random inputs: ", error);
+    }
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  };
+
+  useEffect(() => {
+    fetchRandomInputs();
+  }, []);
 
   return (
     <ScrollView
@@ -41,43 +73,83 @@ export default function HomeScreen({ navigation }) {
           />
         </Box>
 
-        {/* Input */}
-        <Box position="relative" w="80%">
-          <Input
-            variant="outline"
-            placeholder="Type English or Chinese..."
-            onChangeText={handleTextChange}
-            value={query}
-            multiline={true}
-            h={40}
-            paddingX={1}
-            paddingY={2}
-            style={{
-              fontSize: 20,
-              color: colors.onSurface,
-            }}
-          />
-          {query.length > 0 && (
-            <IconButton
-              icon={
-                <Ionicons
-                  name="close-outline"
-                  size={30}
-                  color={colors.onSurfaceVariant}
-                />
-              }
-              position="absolute"
-              top={0}
-              right={0}
-              _hover={{ bg: "transparent" }}
-              _pressed={{ bg: "transparent" }}
-              onPress={() => setQuery("")}
+        {/* Random Words and Input Box */}
+        <Box w="80%">
+          <Flex direction="row" wrap="wrap">
+            <Text
+              fontSize="md"
+              fontWeight="bold"
+              color={colors.onSurface}
+              mr={2}
+              alignSelf="center"
+            >
+              Try:
+            </Text>
+            {randomInputs.map((input, index) => (
+              <Button
+                key={index}
+                onPress={() => setQueryText(input)}
+                borderRadius="full"
+                backgroundColor={colors.primaryContainer}
+                _pressed={{
+                  backgroundColor: colors.onPrimaryContainer,
+                  opacity: 0.8,
+                  _text: {
+                    color: colors.primaryContainer,
+                  },
+                }}
+                _text={{
+                  color: colors.onPrimaryContainer,
+                  fontSize: "xs",
+                  fontWeight: "bold",
+                }}
+                paddingX={2}
+                marginX={1}
+                marginY={1}
+              >
+                {capitalizeFirstLetter(input)}
+              </Button>
+            ))}
+          </Flex>
+
+          {/* Input */}
+          <Box position="relative" mt={3}>
+            <Input
+              variant="outline"
+              placeholder="Type English or Chinese..."
+              onChangeText={handleTextChange}
+              value={queryText}
+              multiline={true}
+              h={40}
+              paddingX={1}
+              paddingY={2}
+              style={{
+                fontSize: 20,
+                color: colors.onSurface,
+              }}
             />
-          )}
+            {queryText.length > 0 && (
+              <IconButton
+                icon={
+                  <Ionicons
+                    name="close-outline"
+                    size={30}
+                    color={colors.onSurfaceVariant}
+                  />
+                }
+                position="absolute"
+                top={0}
+                right={0}
+                _hover={{ bg: "transparent" }}
+                _pressed={{ bg: "transparent" }}
+                onPress={() => setQueryText("")}
+              />
+            )}
+          </Box>
         </Box>
 
         {/* Submit Button */}
-        {query.length > 0 && (
+        {queryText.length > 0 && (
           <Box w="80%" flexDirection="row" justifyContent="flex-end">
             <IconButton
               icon={
@@ -87,7 +159,9 @@ export default function HomeScreen({ navigation }) {
                   color={colors.primaryContainer}
                 />
               }
-              onPress={() => navigation.navigate("Result", { query })}
+              onPress={() =>
+                navigation.navigate("Result", { query: queryText })
+              }
               _hover={{ bg: "transparent" }}
               _pressed={{ bg: "transparent" }}
             />
