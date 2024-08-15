0ded039bbc3b22bfb9a2556367daf4356cb82114
Vincent Zhu <vincenthero16@gmail.com>
2024-08-14 21:05:42 -0400

    feat: in progressing showing quiz score

--- HokkienTranslation/screens/FlashcardAdd.js
+++ HokkienTranslation/screens/FlashcardAdd.js
@@ -1,88 +1,106 @@
 import { useEffect } from "react";
 import { StackActions, useNavigation } from "@react-navigation/native";
 import { useState } from "react";
-import { View, TextInput, Button, FlatList, Text, TouchableOpacity } from "react-native";
-import { doc, getDocs, getDoc, setDoc, collection, query, where, updateDoc, serverTimestamp, deleteDoc } from "firebase/firestore";
+import {
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import {
+  doc,
+  getDocs,
+  getDoc,
+  setDoc,
+  collection,
+  query,
+  where,
+  updateDoc,
+  serverTimestamp,
+  deleteDoc,
+} from "firebase/firestore";
 import { db } from "../backend/database/Firebase";
 import { HStack, Switch } from "native-base";
-
 
 var categoryID = "";
 var ID = "";
 async function getFlashcardsforCategory(db, category) {
-    
-    const categoryRef = collection(db, 'category');
+  const categoryRef = collection(db, "category");
 
-    const q = query(categoryRef, where("name", "==", category));
+  const q = query(categoryRef, where("name", "==", category));
 
-    const querySnapshot = await getDocs(q);
-    var flashcardList = [];
-    var decks = [];
-    console.log(q)
-    querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        decks = doc.data().flashcardList;
-        categoryID = doc.id;
-      });
-    
-     
-      for (const deck of decks) {
-        const deckRef = doc(db, 'flashcardList', deck);
-        const deckDoc = await getDoc(deckRef);
-        const deckData = deckDoc.data();
-        for (const card of deckData.cardList) {
-            flashcardList.push(card);
-        }
-      }
+  const querySnapshot = await getDocs(q);
+  var flashcardList = [];
+  var decks = [];
+  console.log(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    decks = doc.data().flashcardList;
+    categoryID = doc.id;
+  });
 
-    var flashcards = [];
-
-    // filter duplicates (by same id)
-    for (const flashcard of flashcardList) {
-      const flashcardRef = doc(db, 'flashcard', flashcard);
-      const flashcardDoc = await getDoc(flashcardRef);
-      const flashcardData = flashcardDoc.data();
-      console.log(flashcardData)
-      // step could be taken here to ensured we are not accessing other users flashcards
-
-
-      flashcards.push({word: flashcardData.destination, translation: flashcardData.origin, id: flashcardRef.id});
+  for (const deck of decks) {
+    const deckRef = doc(db, "flashcardList", deck);
+    const deckDoc = await getDoc(deckRef);
+    const deckData = deckDoc.data();
+    for (const card of deckData.cardList) {
+      flashcardList.push(card);
     }
-    
-    // ensure unique flashcard id
-    flashcards = flashcards.filter((item, index, self) => 
-        index === self.findIndex((t) => t.id === item.id));    
-    return flashcards;
   }
 
-  
+  var flashcards = [];
+
+  // filter duplicates (by same id)
+  for (const flashcard of flashcardList) {
+    const flashcardRef = doc(db, "flashcard", flashcard);
+    const flashcardDoc = await getDoc(flashcardRef);
+    const flashcardData = flashcardDoc.data();
+    console.log(flashcardData);
+    // step could be taken here to ensured we are not accessing other users flashcards
+
+    flashcards.push({
+      word: flashcardData.destination,
+      translation: flashcardData.origin,
+      id: flashcardRef.id,
+    });
+  }
+
+  // ensure unique flashcard id
+  flashcards = flashcards.filter(
+    (item, index, self) => index === self.findIndex((t) => t.id === item.id)
+  );
+  return flashcards;
+}
 
 const FlashcardAdd = ({ route }) => {
-
-    const navigation = useNavigation();
-  const [deckName, setDeckName] = useState(route.params.deckName || '');
-  const [selectedFlashcards, setSelectedFlashcards] = useState(route.params.selectedFlashcards || []);
-const [flashcards, setFlashcards] = useState([]);
-    const [shared, setShared] = useState(route.params.shared || false);
-
-
-
+  const navigation = useNavigation();
+  const [deckName, setDeckName] = useState(route.params.deckName || "");
+  const [selectedFlashcards, setSelectedFlashcards] = useState(
+    route.params.selectedFlashcards || []
+  );
+  const [flashcards, setFlashcards] = useState([]);
+  const [shared, setShared] = useState(route.params.shared || false);
 
   useEffect(() => {
-    getFlashcardsforCategory(db, route.params.curCategory).then((flashcardList) => {
-      setFlashcards(flashcardList);
-    }).catch((error) => {
-      console.error("Error fetching flashcards: ", error);
-    });
+    getFlashcardsforCategory(db, route.params.curCategory)
+      .then((flashcardList) => {
+        setFlashcards(flashcardList);
+      })
+      .catch((error) => {
+        console.error("Error fetching flashcards: ", error);
+      });
   }, []);
 
   const toggleSelection = (id) => {
     if (selectedFlashcards.includes(id)) {
-      setSelectedFlashcards(selectedFlashcards.filter(flashcardId => flashcardId !== id));
+      setSelectedFlashcards(
+        selectedFlashcards.filter((flashcardId) => flashcardId !== id)
+      );
     } else {
       setSelectedFlashcards([...selectedFlashcards, id]);
     }
-   
   };
 
   const handleSubmission = async () => {
@@ -90,96 +108,92 @@ const [flashcards, setFlashcards] = useState([]);
 
     var cardList = selectedFlashcards;
 
-    console.log(route.params.update)
+    console.log(route.params.update);
 
     if (route.params.update) {
-        var serverTimestamps;
-        await getDoc(doc(db, 'flashcardList', route.params.deckName)).then((doc) => {
-            console.log(doc.data())
-            serverTimestamps = doc.data().createdAt;
-            console.log(serverTimestamps)
+      var serverTimestamps;
+      await getDoc(doc(db, "flashcardList", route.params.deckName)).then(
+        (doc) => {
+          console.log(doc.data());
+          serverTimestamps = doc.data().createdAt;
+          console.log(serverTimestamps);
         }
-        )
-        console.log(serverTimestamps)
-        await setDoc(doc(db, 'flashcardList', deckName), {
-            name: deckName,
-            cardList: cardList,
-            createdAt: serverTimestamps,
-            categoryID: categoryID,
-            createdBy: route.params.currentUser, 
-            shared: shared
-        });
+      );
+      console.log(serverTimestamps);
+      await setDoc(doc(db, "flashcardList", deckName), {
+        name: deckName,
+        cardList: cardList,
+        createdAt: serverTimestamps,
+        categoryID: categoryID,
+        createdBy: route.params.currentUser,
+        shared: shared,
+      });
 
-        await deleteDoc(doc(db, 'flashcardList', route.params.deckName));
-        const categoryRef2 = doc(db, 'category', categoryID);
-        const categoryDoc2 = await getDoc(categoryRef2);
-        const categoryData2 = categoryDoc2.data();
-        var flashcardList = categoryData2.flashcardList;
-        flashcardList.splice(flashcardList.indexOf(route.params.deckName), 1);
-        flashcardList.push(deckName);
+      await deleteDoc(doc(db, "flashcardList", route.params.deckName));
+      const categoryRef2 = doc(db, "category", categoryID);
+      const categoryDoc2 = await getDoc(categoryRef2);
+      const categoryData2 = categoryDoc2.data();
+      var flashcardList = categoryData2.flashcardList;
+      flashcardList.splice(flashcardList.indexOf(route.params.deckName), 1);
+      flashcardList.push(deckName);
 
       // update caategory
       await updateDoc(categoryRef2, {
-        flashcardList: flashcardList
+        flashcardList: flashcardList,
       });
-    }
-    else {
-    setDoc(doc(db, 'flashcardList', deckName), {
+    } else {
+      setDoc(doc(db, "flashcardList", deckName), {
         name: deckName,
         cardList: cardList,
         createdAt: serverTimestamp(),
         categoryID: categoryID,
         createdBy: route.params.currentUser, // TODO: placeholder get from auth
-        shared: shared //add a shared setting to this later.
-    });
+        shared: shared, //add a shared setting to this later.
+      });
 
-    // have to add a new deck to the category
+      // have to add a new deck to the category
 
-
-    const categoryRef = doc(db, 'category', categoryID);
-    const categoryDoc = await getDoc(categoryRef);
-    const categoryData = categoryDoc.data();
-    var flashcardList = categoryData.flashcardList;
-    flashcardList.push(deckName);
-    updateDoc(categoryRef, {
+      const categoryRef = doc(db, "category", categoryID);
+      const categoryDoc = await getDoc(categoryRef);
+      const categoryData = categoryDoc.data();
+      var flashcardList = categoryData.flashcardList;
+      flashcardList.push(deckName);
+      updateDoc(categoryRef, {
         flashcardList: flashcardList,
-    });
+      });
 
-
-    // manipualted selectedFlashcards to get ids
-    console.log(selectedFlashcards);
+      // manipualted selectedFlashcards to get ids
+      console.log(selectedFlashcards);
     }
 
     // await setDoc(doc(db, 'flashcardList', deckName), {
     //     name: deckName,
     //     flashcardList: selectedFlashcards
     // });
-    
-    var flashcardList = cardList
-    cardList = []
-    for (const card of flashcardList) {
-        console.log(card);
-    
-        // Reference to the document
-          // Assuming the flashcardData has a cardList array
-      
-        const cardRef = doc(db, 'flashcard', card);
-        const cardDoc = await getDoc(cardRef);
-        
-  
-        // TODO: check for auth
-          if (cardDoc.exists()) {
-            const cardData = cardDoc.data();
-            cardList.push({
-              word: cardData.destination,
-              translation: cardData.origin
-            });
-          }
-        }
-    navigation.navigate('Flashcard', { cardList });
-    //navigation.dispatch(StackActions.replace('Category'));
-  }
 
+    var flashcardList = cardList;
+    cardList = [];
+    for (const card of flashcardList) {
+      console.log(card);
+
+      // Reference to the document
+      // Assuming the flashcardData has a cardList array
+
+      const cardRef = doc(db, "flashcard", card);
+      const cardDoc = await getDoc(cardRef);
+
+      // TODO: check for auth
+      if (cardDoc.exists()) {
+        const cardData = cardDoc.data();
+        cardList.push({
+          word: cardData.destination,
+          translation: cardData.origin,
+        });
+      }
+    }
+    navigation.navigate("Flashcard", { cardList });
+    //navigation.dispatch(StackActions.replace('Category'));
+  };
 
   return (
     <View style={{ padding: 20 }}>
@@ -190,16 +204,37 @@ const [flashcards, setFlashcards] = useState([]);
         style={{ borderWidth: 1, padding: 10, marginBottom: 20 }}
       />
       <HStack style={{ flex: 1, marginBottom: 20 }}>
-    <Text>Share</Text>
-      <Switch title="Share" isChecked={shared} onToggle={() => {setShared(!shared)}} style={{marginLeft: 10}}/>   
+        <Text>Share</Text>
+        <Switch
+          title="Share"
+          isChecked={shared}
+          onToggle={() => {
+            setShared(!shared);
+          }}
+          style={{ marginLeft: 10 }}
+        />
       </HStack>
-      <Button title="Submit" onPress={() => {handleSubmission()}} />
+      <Button
+        title="Submit"
+        onPress={() => {
+          handleSubmission();
+        }}
+      />
       <FlatList
         data={flashcards}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <TouchableOpacity onPress={() => toggleSelection(item.id)}>
-            <View style={{ flexDirection: 'row', padding: 10, alignItems: 'center', backgroundColor: selectedFlashcards.includes(item.id) ? 'lightblue' : 'white' }}>
+            <View
+              style={{
+                flexDirection: "row",
+                padding: 10,
+                alignItems: "center",
+                backgroundColor: selectedFlashcards.includes(item.id)
+                  ? "lightblue"
+                  : "white",
+              }}
+            >
               <Text style={{ flex: 1 }}>{item.word}</Text>
               <Text style={{ flex: 1 }}>{item.translation}</Text>
               <View
@@ -207,8 +242,12 @@ const [flashcards, setFlashcards] = useState([]);
                   width: 24,
                   height: 24,
                   borderWidth: 2,
-                  borderColor: selectedFlashcards.includes(item.id) ? 'green' : 'gray',
-                  backgroundColor: selectedFlashcards.includes(item.id) ? 'green' : 'white',
+                  borderColor: selectedFlashcards.includes(item.id)
+                    ? "green"
+                    : "gray",
+                  backgroundColor: selectedFlashcards.includes(item.id)
+                    ? "green"
+                    : "white",
                 }}
               />
             </View>
@@ -217,6 +256,6 @@ const [flashcards, setFlashcards] = useState([]);
       />
     </View>
   );
-}
+};
 
 export default FlashcardAdd;
