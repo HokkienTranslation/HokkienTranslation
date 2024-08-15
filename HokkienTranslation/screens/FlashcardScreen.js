11c63849aeca910b6aa4a016001973bc00323a7f
Vincent Zhu <vincenthero16@gmail.com>
2024-08-14 20:39:23 -0400

    feat: Quiz shows the current flashcardList dynamically

--- HokkienTranslation/screens/FlashcardScreen.js
+++ HokkienTranslation/screens/FlashcardScreen.js
@@ -1,5 +1,16 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Box, Text, Button, Center, VStack, HStack, Pressable, Input, Select,} from "native-base";
+import {
+  Box,
+  Text,
+  Button,
+  Center,
+  VStack,
+  HStack,
+  Pressable,
+  Input,
+  Select,
+} from "native-base";
+
 import { Ionicons } from "@expo/vector-icons";
 import { TouchableOpacity, Modal, Animated, PanResponder } from "react-native";
 import NavigationButtons from "../screens/components/ScreenNavigationButtons";
@@ -175,18 +186,14 @@ const FlashcardScreen = ({ route, navigation }) => {
       <Center flex={1} px="3">
         <VStack space={4} alignItems="center">
           <HStack space={4}>
+            <CrudButtons title="Create" onPress={handleCreate} iconName="add" />
             <CrudButtons
-              title="Create" 
-              onPress={handleCreate} 
-              iconName="add"
-            />
-            <CrudButtons
-              title="Update" 
-              onPress={handleUpdate} 
+              title="Update"
+              onPress={handleUpdate}
               iconName="pencil"
             />
             <CrudButtons
-              title="Delete" 
+              title="Delete"
               onPress={() => setShowConfirmDelete(true)}
               iconName="trash"
             />
@@ -362,15 +369,15 @@ const FlashcardScreen = ({ route, navigation }) => {
                 </Box>
                 <HStack space={4}>
                   <CrudButtons
-                      title="Save"
-                      iconName="save"
-                      onPress={handleCreate}
-                    />
-                    <CrudButtons
-                      title="Cancel"
-                      iconName="close"
-                      onPress={() => setShowNewFlashcard(false)}
-                    />
+                    title="Save"
+                    iconName="save"
+                    onPress={handleCreate}
+                  />
+                  <CrudButtons
+                    title="Cancel"
+                    iconName="close"
+                    onPress={() => setShowNewFlashcard(false)}
+                  />
                 </HStack>
               </VStack>
             </Box>
@@ -404,10 +411,16 @@ const FlashcardScreen = ({ route, navigation }) => {
                 Edit flashcard:
               </Text>
               <VStack space={4} alignItems="center">
-                <Input placeholder="Enter word" width={200} 
-                 value={flashcards[currentCardIndex].word}/>
-                <Input placeholder="Enter Translation" width={200} 
-                 value={flashcards[currentCardIndex].translation}/>
+                <Input
+                  placeholder="Enter word"
+                  width={200}
+                  value={flashcards[currentCardIndex].word}
+                />
+                <Input
+                  placeholder="Enter Translation"
+                  width={200}
+                  value={flashcards[currentCardIndex].translation}
+                />
                 <Box space={4} alignItems="center" width={200}>
                   <Select
                     selectedValue={otherOpt}
@@ -508,16 +521,16 @@ const FlashcardScreen = ({ route, navigation }) => {
                 Delete this flashcard?
               </Text>
               <HStack space={4}>
-                  <CrudButtons
-                    title="Yes"
-                    iconName="checkmark"
-                    onPress={handleDelete}
-                  />
-                  <CrudButtons
-                    title="No"
-                    iconName="close"
-                    onPress={() => setShowConfirmDelete(false)}
-                  />
+                <CrudButtons
+                  title="Yes"
+                  iconName="checkmark"
+                  onPress={handleDelete}
+                />
+                <CrudButtons
+                  title="No"
+                  iconName="close"
+                  onPress={() => setShowConfirmDelete(false)}
+                />
               </HStack>
             </Box>
           </Center>
