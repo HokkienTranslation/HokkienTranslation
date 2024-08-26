5f61b898d96cad0e409a5a59235e56c5cc010504
tasmimabrar <tasmimabrar@gmail.com>
2024-08-26 00:55:27 -0400

    fix: correct values are now passed to flashcardScreen

--- HokkienTranslation/screens/FlashcardAdd.js
+++ HokkienTranslation/screens/FlashcardAdd.js
@@ -25,7 +25,7 @@ import { db } from "../backend/database/Firebase";
 import { HStack, Switch } from "native-base";
 import { useTheme } from "./context/ThemeProvider";
 
-var categoryID = "";
+var categoryId = "";
 var ID = "";
 async function getFlashcardsforCategory(db, category) {
   const categoryRef = collection(db, "category");
@@ -39,7 +39,7 @@ async function getFlashcardsforCategory(db, category) {
   querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
     decks = doc.data().flashcardList;
-    categoryID = doc.id;
+    categoryId = doc.id;
   });
 
   for (const deck of decks) {
@@ -66,6 +66,7 @@ async function getFlashcardsforCategory(db, category) {
       translation: flashcardData.origin,
       id: flashcardRef.id,
     });
+    //console.log(flashcardRef.id)
   }
 
   // ensure unique flashcard id
@@ -86,6 +87,9 @@ const FlashcardAdd = ({ route }) => {
 
   const { theme, themes } = useTheme();
   const colors = themes[theme];
+
+  const categoryId = route.params.categoryId;
+  console.log("Category ID in FlashcardAdd: ", categoryId)
 
   useEffect(() => {
     getFlashcardsforCategory(db, route.params.curCategory)
@@ -131,12 +135,12 @@ const FlashcardAdd = ({ route }) => {
         name: deckName,
         cardList: cardList,
         createdAt: serverTimestamps,
-        categoryId: categoryID,
+        categoryId: categoryId,
         createdBy: route.params.currentUser,
         shared: shared,
       });
 
-      const categoryRef2 = doc(db, "category", categoryID);
+      const categoryRef2 = doc(db, "category", categoryId);
       const categoryDoc2 = await getDoc(categoryRef2);
       const categoryData2 = categoryDoc2.data();
       var flashcardList = categoryData2.flashcardList;
@@ -153,13 +157,13 @@ const FlashcardAdd = ({ route }) => {
         name: deckName,
         cardList: cardList,
         createdAt: serverTimestamp(),
-        categoryId: categoryID,
+        categoryId: categoryId,
         createdBy: route.params.currentUser,
         shared: shared,
       });
 
       // Update the category
-      const categoryRef = doc(db, "category", categoryID);
+      const categoryRef = doc(db, "category", categoryId);
       const categoryDoc = await getDoc(categoryRef);
       const categoryData = categoryDoc.data();
       var flashcardList = categoryData.flashcardList;
@@ -187,6 +191,11 @@ const FlashcardAdd = ({ route }) => {
       if (cardDoc.exists()) {
         const cardData = cardDoc.data();
         cardList.push({
+          id: card,
+          origin: cardData.origin,
+          destination: cardData.destination,
+          otherOptions: cardData.otherOptions,
+          type: cardData.type,
           word: cardData.destination,
           translation: cardData.origin,
         });
