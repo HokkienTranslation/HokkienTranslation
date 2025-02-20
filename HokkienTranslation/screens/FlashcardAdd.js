90d7dddb07d91f1d90f7b3d3c44142bc54a9c46f
Lucie Yang <luya.495@gmail.com>
2025-02-20 02:09:25 -0500

    Add comment on sharing

--- HokkienTranslation/screens/FlashcardAdd.js
+++ HokkienTranslation/screens/FlashcardAdd.js
@@ -265,6 +265,9 @@ const FlashcardAdd = ({ route }) => {
               style={{ marginLeft: 10 }}
               onTrackColor={colors.primary}
             />
+            <Text style={{ color: colors.onSurfaceVariant, marginLeft: 10, fontSize: 12}}>
+              Sharing means anyone can view or edit your deck.
+            </Text>
           </HStack>
           <Text>Select at least one base flashcard. You can create custom flashcards on the next page.</Text>
           <TouchableOpacity
