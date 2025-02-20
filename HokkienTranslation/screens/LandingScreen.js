3a38ea67ad12c3c8f09ab58875f0720e6a62327c
Lucie Yang <luya.495@gmail.com>
2025-02-19 19:52:50 -0500

    Improved aesthetics of landing page

--- HokkienTranslation/screens/LandingScreen.js
+++ HokkienTranslation/screens/LandingScreen.js
@@ -1,9 +1,23 @@
-import React from "react";
-import { ImageBackground, Pressable } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { ImageBackground, Pressable, Animated } from "react-native";
 import { Box, Image, Text, VStack } from "native-base";
 import { CommonActions } from "@react-navigation/native";
+import { useTheme } from "./context/ThemeProvider";
 
 const LandingPage = ({ navigation }) => {
+  const { theme, themes } = useTheme();
+  const colors = themes[theme];
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 800, // 0.8 seconds
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
   return (
     <Pressable
       style={{ flex: 1 }}
@@ -19,25 +33,37 @@ const LandingPage = ({ navigation }) => {
     >
       <ImageBackground
         source={require("../assets/background.png")}
-        style={{ flex: 1, justifyContent: "flex-start", alignItems: "center" }}
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
       >
-        <Box
+
+      <Box
+          position="absolute"
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          backgroundColor={`${colors.primaryContainer}C0`}
+      />
+
+        {/* <Box
           marginX="20%"
           marginY="5%"
           justifyContent="center"
           alignItems="center"
           width="100%"
-        >
+        > */}
+        <Animated.View style={{ opacity: fadeAnim, alignItems: "center", width: "80%", marginTop: "-20%"}}>
           <Image source={require("../assets/logo.png")} alt="Logo" size="2xl" />
           <VStack space={2} alignItems="center">
-            <Text fontSize="xl" color="gray.400">
+            <Text fontSize="2xl"  fontWeight="bold" color={colors.onSurface} textAlign="center">
               Hokkien Translation & Education Tool
             </Text>
-            <Text fontSize="md" color="gray.400">
+            <Text fontSize="md" color={colors.onPrimaryContainer} textAlign="center">
               Tap anywhere to continue
             </Text>
           </VStack>
-        </Box>
+        {/* </Box> */}
+        </Animated.View>
       </ImageBackground>
     </Pressable>
   );
